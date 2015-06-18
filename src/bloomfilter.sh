e2b4d07ff320b981a5f5dcbf813e5ca71b78eea3
Troy Daniels <troy_daniels@live.com.au>
2015-06-19 01:56:10 +1000

    Working implementation

--- src/bloomfilter.sh
+++ src/bloomfilter.sh
@@ -3,29 +3,34 @@
 #A bloom filter, using functions from bloomlib.sh
 . bloomlib.sh
 
-declare -i bloom_filter=""
+declare -i bloom_filter=0
 declare -i k_value=5
+declare -i element_test
+declare  add="add"
+declare  test="test"
 
+echo "Usage: [$add|$test] ELEMENT"
 while :
 do
+  read flag element
   #Validate input
-  while [[ "$flag" != "a" && "$flag" != "t" ]]
-  do
-    echo "Usage: arg [a|t] - a for add, t for test"
-  read element flag
-  done
-
-  #Perform operation
-  if [ "$flag" = "a" ]
+  if [[ "$flag" != "$add" && "$flag" != "$test" ]]
   then
-    add_value "$element" $k_value $bloom_filter
-  else
+    echo "Usage: [$add|$test] ELEMENT"
+    read flag element
+  #Perform operation
+  elif [ "$flag" = "$add" ]
+  then
+    ((bloom_filter=$(add_value "$element" "$k_value" "$bloom_filter")))
+  elif [ "$flag" = "$test" ]
+  then
     #Test existence of element
-    if [ test_value "$element" $k_value $bloom_filter ]
+    ((element_test=$(test_value "$element" "$k_value" "$bloom_filter")))
+    if [[ "$element_test" -eq 0 ]]
     then
-      echo "$element has probably been seen"
+      echo ">$element has probably been seen"
     else
-      echo "$element has not been seen"
+      echo ">$element has not been seen"
     fi
   fi
 
--- src/bloomlib.sh
+++ src/bloomlib.sh
@@ -1,35 +1,33 @@
-#!/bin/bash
 #bloomlib.sh
 #Functions for the manipulation of a bloom filter
 
 #Calculates the 32-bit FNV hash of argument
 #Exits with status 1 if invalid arguments
 #fnv_la_hash element
-
 function fnv_1a ()
 {
-  echo "$1 --" 
-  echo "$# -"
   if [[ -z $1 || $# -ne 1 ]]
   then
       echo "$FUNCNAME: invalid arguments"
       exit 1
   fi
 
-  local 32_fnv_prime=16777619
-  local 32_offset_basis=2166136261
-  local element=$1
-  local string=$32_offset_basis
+  local fnv_prime_32=16777619
+  local offset_basis_32=2166136261
+  local element="$1"
+  local hash
 
   while [ -n "$element" ]
   do
-    hash=$(( $string ^ "${element:0}" ))
-    hash=$string*$32_fnv_prime
-    #Remove first element
-    element="${element:0:${#element}}"
+    hash="$offset_basis"
+    #The offset basis bitwise or equalled with the first character
+    ((hash|=$(ascii_to_dec "${element:0}")))
+    ((hash*="$fnv_prime_32"))
+    #Remove first character
+    element="${element:1:${#element}}"
   done
 
-  return $string
+  echo "$hash"
 }
 
 #Adds k hashed values of an element to a hashstring, all passed in as arguments
@@ -49,20 +47,19 @@ function add_value ()
   local new_hash
 
   #Add k different hashes of character to string
-  for count in {1..k_value}
+  while [ "$k_value" -ne 0 ]
   do
     #g(x)=f(x)+i*h(x)
     #bitwise AND together to give FNV hash-bit length string
-    fnv_1a "$element"
-    new_hash=$?
-    string=$string & ($new_hash + $count * $new_hash)
+    ((new_hash=$(fnv_1a "$element")))
+    ((string|=("$new_hash"+"$k_value"*"$new_hash")))
+    ((k_value--))
   done
-
-  return $string
+  echo "$string"
 }
 
 #Tests to see if element is a member of a hashstring as defined by add_element
-#Returns 1 if true, 0 if false, and exits with status 1 if incorrect number of arguments
+#Returns 0 IF TRUE, the hash of the element if false, and exits with status 1 if incorrect number of arguments
 #test_value element k string
 function test_value ()
 {
@@ -79,16 +76,45 @@ function test_value ()
   local compare
 
   #Add k different hashes of character to test
-  for count in {1..k_value}
+  while [ "$k_value" -ne 0 ]
   do
     #g(x)=f(x)+i*h(x)
     #bitwise AND together to give FNV hash-bit length string
-    test=$test & (fnv_1a "$element" + $count * fnv_1a "$element")
+    ((new_hash=$(fnv_1a "$element")))
+    ((test|=("$new_hash"+"$k_value"*"$new_hash")))
+    ((k_value--))
   done
 
-  #If strings can be bitwise ANDed together, and are equal to hashstring, we have a high
+  #If strings can be bitwise ANDed together, and are equal to original test hash, we have a high
   #probability of having a match
-  compare=$string & $test
-  return [ $compare -eq $string ] && 1 || 0
+  ((compare="$string" & "$test"))
+  ((compare^="$test"))
+#  verbose "$string" "$test" "$compare"
+  echo "$compare"
 }
 
+#Returns decimal value of ASCII character passed in as argument
+#Exits with status 1 if passed invalid arguments
+function ascii_to_dec()
+{
+
+  if [[ -z $1 || $# -ne 1 ]]
+  then
+#      echo "$FUNCNAME: invalid arguments"
+      exit 1
+  fi
+
+  local dec_val
+
+  #Note seemingly (but not) misplaced ' character
+  printf -v dec_val %d "'$1"
+
+  echo "$dec_val"
+}
+
+function verbose()
+{
+  echo "Current String:   $1"
+  echo "Element Hash:     $2"
+  echo "Difference:       $3"
+}
